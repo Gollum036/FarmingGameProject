c75008f54a20405156716ffe6f8750e08eb18bca
Gollum036 <pedromiguel.dfonseca@gmail.com>
2024-03-15 12:27:13 +0000

    Continuação do Player Controller
    Adicionei RigidBody 2D;
    Adicionei movimento vertical;
    Retirei a gravidade ao Player;
    Problema: Movimento fluído e não por unidades...

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float movimentox;
+    public float movimentoy;
+    public float movSpeed;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movimentox = Input.GetAxis("Horizontal");
+        movimentox = Input.GetAxis("Horizontal") * movSpeed;
+        movimentoy = Input.GetAxis("Vertical") * movSpeed;
+        rb.velocity = new Vector2(movimentox, movimentoy);
     }
 }
