d9fa41a7eac84a5d8cde348b6463fedb4e6573e1
Gollum036 <pedromiguel.dfonseca@gmail.com>
2024-04-18 00:26:54 +0100

    ahhhh
    apaguei os scripts dos itens, visto que não tava a dar e fiz de novo num só script. Falta adicionar os itens e testar.

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -1,14 +1,126 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
 
-[CreateAssetMenu]
-public class ItemsData : ScriptableObject
+public class Inventory : MonoBehaviour
 {
-    public string itemName;
-    public Sprite icon;
-    [TextArea]
-    public string description;
+    public List<Item> items = new List<Item>();
+    public Transform itemsParent;
+    public GameObject inventoryUI;
+    public Text itemNameText;
+    public Text itemDescriptionText;
 
-    // O CreateAssetMenu permite no Unity criar Item Data objects na pasta dos assets.
+    public Item selectedItem;
+
+    public ItemDatabase itemDatabase;
+
+    public InventorySlot[] slots;
+
+    void Start()
+    {
+        itemDatabase = GetComponent<ItemDatabase>();
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleInventory();
+        }
+    }
+
+    public void AddItem(int id)
+    {
+        Item itemToAdd = itemDatabase.GetItemById(id);
+        items.Add(itemToAdd);
+
+        UpdateUI();
+    }
+
+    public void RemoveItem(int id)
+    {
+        Item itemToRemove = items.Find(item => item.id == id);
+        if (itemToRemove != null)
+        {
+            items.Remove(itemToRemove);
+        }
+
+        UpdateUI();
+    }
+
+    public void SelectItem(Item item)
+    {
+        selectedItem = item;
+        itemNameText.text = selectedItem.name;
+        itemDescriptionText.text = selectedItem.description;
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < items.Count)
+            {
+                slots[i].AddItem(items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+
+    void ToggleInventory()
+    {
+        inventoryUI.SetActive(!inventoryUI.activeSelf);
+    }
+}
+
+[Serializable]
+public class Item
+{
+    public int id;
+    public string name;
+    public string description;
+    public int price;
+    public Sprite icon; // Added icon field
+    // Add any other properties you need for your items here
+}
+
+[Serializable]
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    Item item;
+
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+        icon.sprite = item.icon;
+        icon.enabled = true;
+    }
+
+    public void ClearSlot()
+    {
+        item = null;
+        icon.sprite = null;
+        icon.enabled = false;
+    }
+}
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    void Start()
+    {
+        // Initialize your items here
+    }
+
+    public Item GetItemById(int id)
+    {
+        return items.Find(item => item.id == id);
+    }
 }
