005db2d9d2f54f23fafa238126b9a218f2b27c32
Gollum036 <pedromiguel.dfonseca@gmail.com>
2024-04-18 00:57:28 +0100

    Inventory continuation
    Tentar mudar isto para que possa criar os itens diretamente no unity...

---
+++ Assets/Scripts/inventory.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+    public Transform itemsParent;
+    public GameObject inventoryUI;
+    public Text itemNameText;
+    public Text itemDescriptionText;
+
+    public Item selectedItem;
+
+    public InventorySlot[] slots;
+
+    void Start()
+    {
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleInventory();
+        }
+    }
+
+    public void AddItem(ItemData itemData)
+    {
+        Item newItem = new Item(itemData);
+        items.Add(newItem);
+
+        UpdateUI();
+    }
+
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+        UpdateUI();
+    }
+
+    public void SelectItem(Item item)
+    {
+        selectedItem = item;
+        itemNameText.text = selectedItem.itemData.name;
+        itemDescriptionText.text = selectedItem.itemData.description;
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < items.Count)
+            {
+                slots[i].AddItem(items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+
+    void ToggleInventory()
+    {
+        inventoryUI.SetActive(!inventoryUI.activeSelf);
+    }
+}
+
+[System.Serializable]
+public class Item
+{
+    public ItemData itemData;
+
+    public Item(ItemData data)
+    {
+        itemData = data;
+    }
+}
+
+[System.Serializable]
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    Item item;
+
+    public void AddItem(Item newItem)
+    {
+        item = newItem;
+        icon.sprite = newItem.itemData.icon;
+        icon.enabled = true;
+    }
+
+    public void ClearSlot()
+    {
+        item = null;
+        icon.sprite = null;
+        icon.enabled = false;
+    }
+}
