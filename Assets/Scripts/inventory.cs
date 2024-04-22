950cbf7fd7a675143483df18eb66009743d154e2
Gollum036 <pedromiguel.dfonseca@gmail.com>
2024-04-22 15:08:44 +0100

    Inventory Toggle

--- Assets/Scripts/inventory.cs
+++ Assets/Scripts/inventory.cs
@@ -3,24 +3,9 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
 
-public class Inventory : MonoBehaviour
+public class InventoryUI : MonoBehaviour
 {
-    public GameObject Slots;
-    public List<Item> items = new List<Item>();
-    public Transform itemsParent;
-    public GameObject inventoryUI;
-    public Text itemNameText;
-    public Text itemDescriptionText;
     public GameObject inventoryPanel;
-
-    public Item selectedItem;
-
-    public InventorySlot[] slots;
-
-    void Start()
-    {
-        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
-    }
 
     void Update()
     {
@@ -29,79 +14,10 @@ public class Inventory : MonoBehaviour
             ToggleInventory();
         }
     }
-    
-    public void AddItem(ItemData itemData)
-    {
-        Item newItem = new Item(itemData);
-        items.Add(newItem);
-
-        UpdateUI();
-    }
-
-    public void RemoveItem(Item item)
-    {
-        items.Remove(item);
-        UpdateUI();
-    }
-
-    public void SelectItem(Item item)
-    {
-        selectedItem = item;
-        itemNameText.text = selectedItem.itemData.name;
-        itemDescriptionText.text = selectedItem.itemData.description;
-    }
-
-    void UpdateUI()
-    {
-        for (int i = 0; i < slots.Length; i++)
-        {
-            if (i < items.Count)
-            {
-                slots[i].AddItem(items[i]);
-            }
-            else
-            {
-                slots[i].ClearSlot();
-            }
-        }
-    }
 
     public void ToggleInventory()
     {
-        
-        inventoryUI.SetActive(!inventoryUI.activeSelf);
-    }
-}
 
-[System.Serializable]
-public class Item
-{
-    public ItemData itemData;
-
-    public Item(ItemData data)
-    {
-        itemData = data;
-    }
-}
-
-[System.Serializable]
-public class InventorySlot : MonoBehaviour
-{
-    public Image icon;
-    
-    Item item;
-
-    public void AddItem(Item newItem)
-    {
-        item = newItem;
-        icon.sprite = newItem.itemData.icon;
-        icon.enabled = true;
-    }
-
-    public void ClearSlot()
-    {
-        item = null;
-        icon.sprite = null;
-        icon.enabled = false;
+        inventoryPanel.SetActive(!inventoryPanel.activeSelf);
     }
 }
